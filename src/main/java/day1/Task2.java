8c99ad97fd8194f5b2c02d8f13b6cf77910b52d4
Jannik Götte <anfrage@jannik-goette.de>
2024-12-09 08:12:27 +0100

    Fix imports

--- src/main/java/day1/Task2.java
+++ src/main/java/day1/Task2.java
@@ -1,11 +1,9 @@
 package day1;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Task2 {
